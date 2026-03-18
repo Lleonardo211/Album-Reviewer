a792204aae7eb171e4225ad9608e23f84abb70be
Leonte Leonardo-Ioan <leonardoleonte7173@gmail.com>
2026-03-18 20:10:24 +0200

    added all Album class constructors, added a title card

--- project.cpp
+++ project.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <windows.h>
 using namespace std;
 
 class Album{
@@ -12,23 +13,28 @@ class Album{
         int length;
         
     public:
+        Album();
         Album(char * title, char * bandName, char * genre, int length);
+        Album(const Album&);
+        Album& operator=(const Album& obj);
         ~Album();   
 };
 
-Album::Album(char * title, char * bandName, char * genre, int length) : id(albumCnt++) {
-    this->title = new char[strlen(title) + 1];
-    this->bandName = new char[strlen(bandName) + 1];
-    this->genre = new char[strlen(genre) +1];
+int Album::albumCnt=1;
 
-    strcpy(this->title,title);
-    strcpy(this->bandName,bandName);
-    strcpy(this->genre,genre);
-
-    this->length=length;    
+Album::Album() : id(albumCnt++){
+    this->title = strcpy(new char[4], "N/A");
+    this->bandName = strcpy(new char[4], "N/A");
+    this->genre = strcpy(new char[4], "N/A");
+    this->length=0;
 }
 
-int Album::albumCnt=1;
+Album::Album(char * title, char * bandName, char * genre, int length) : id(albumCnt++) {
+    this->title = strcpy(new char[strlen(title) + 1], title);
+    this->bandName = strcpy(new char[strlen(bandName) + 1], bandName);
+    this->genre = strcpy(new char[strlen(genre) +1], genre);
+    this->length=length;    
+}
 
 Album::~Album(){
     delete[] title;
@@ -36,7 +42,51 @@ Album::~Album(){
     delete[] genre;
 }
 
-int main(){
-    
+Album::Album(const Album &obj) : id(albumCnt++) {
+    this->title = strcpy(new char[strlen(obj.title) + 1], obj.title);
+    this->bandName = strcpy(new char[strlen(obj.bandName)+1], obj.bandName);
+    this->genre = strcpy(new char[strlen(obj.genre)+1], obj.genre);
+    this->length = obj.length;
+}
+
+Album& Album::operator=(const Album& obj) {
+    if (this == &obj) {
+        return *this;
+    }
+    delete[] this->title;
+    this->title = strcpy(new char[strlen(obj.title)+1],obj.title);
+    delete[] this->bandName;
+    this->bandName = strcpy(new char[strlen(obj.bandName)+1],obj.bandName);
+    delete[] this->genre;
+    this->genre = strcpy(new char[strlen(obj.genre)+1],obj.genre);
+    this->length=obj.length;
+    return *this;
+}
+
+int main(){  
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+cout<<R"(   
+                                                                                                
+                                                                                               $$$$$$"
+      __       __      .______    __    __  .___  ___.                                        3$$$$$"
+    /   \     |  |     |   _  \  |  |  |  | |   \/   |                                       z$$$$P
+   /  ^  \    |  |     |  |_)  | |  |  |  | |  \  /  |                                      d$$$$"
+  /  /_\  \   |  |     |   _  <  |  |  |  | |  |\/|  |                                    .$$$$$"
+ /  _____  \  |  `----.|  |_)  | |  `--'  | |  |  |  |                                   z$$$$$"
+/__/     \__\ |_______||______/   \______/  |__|  |__|                                  z$$$$P
+                                                                                       d$$$$$$$$$$"
+.______       ___________    ____  __   ___________    __    ____   _______.          *******$$$"
+|   _  \     |   ____\   \  /   / |  | |   ____\   \  /  \  /   /  /       |              .$$$"
+|  |_)  |    |  |__   \   \/   /  |  | |  |__   \   \/    \/   /  |   (----`             .$$"
+|      /     |   __|   \      /   |  | |   __|   \            /    \   \                4$P"
+|  |\  \----.|  |____   \    /    |  | |  |____   \    /\    / .----)   |              z$"
+| _| `._____||_______|   \__/     |__| |_______|   \__/  \__/  |_______/              zP
+                                                                                     z"
+                                                                                    /
+
+)";
+SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
     return 0;
 }
