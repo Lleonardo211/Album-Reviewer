60a08c7f4735b97b07ebbf6161f020d7a9db11b2
Leonte Leonardo-Ioan <leonardoleonte7173@gmail.com>
2026-03-21 20:27:59 +0200

    Added functionality to the Album and Song classes, with the userMenu, added Setters and Getters for the User class

--- project.cpp
+++ project.cpp
@@ -5,7 +5,7 @@
 
 class Album{
     private:
-        int static albumCnt;
+        static int albumCnt;
         const int id;
         char* title;
         char* bandName;
@@ -99,8 +99,15 @@ std::istream& operator>>(std::istream& in, Album& obj){
     }
     obj.format = c;
 
-    std::cout<<" Length: ";
-    in>>obj.length;
+    std::cout<<" Length(minutes): ";
+    while(!(in>>obj.length) || obj.length < 0){
+        in.clear();
+        in.ignore(1000, '\n');
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+        std::cout<<" Invalid input! Please enter a valid length: ";
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    }
     in.ignore();
 
     return in;
@@ -109,15 +116,15 @@ std::istream& operator>>(std::istream& in, Album& obj){
 std::ostream& operator<<(std::ostream& out, const Album& obj){
     out<<" Album #"<<obj.id<<'\n';
     if(obj.title != nullptr)
-        out<<" Title: "<<*obj.title<<'\n';
+        out<<" Title: "<<obj.title<<'\n';
     else    
         out<<"\n No title registered!";
     if(obj.bandName != nullptr)
-        out<<" Band Name: "<<*obj.bandName<<'\n';
+        out<<" Band Name: "<<obj.bandName<<'\n';
     else    
         out<<"\n Band name not registered!";
-    if(obj.title != nullptr)
-        out<<" Genre: "<<*obj.genre<<'\n';
+    if(obj.genre != nullptr)
+        out<<" Genre: "<<obj.genre<<'\n';
     else    
         out<<"\n Genre notregistered!";
     out<<" Format: "<<obj.format<<'\n';
@@ -127,7 +134,7 @@ std::ostream& operator<<(std::ostream& out, const Album& obj){
 
 class Song{
     private:
-        int static songCnt;
+        static int songCnt;
         const int id;
         char* name;
         char* albumName;
@@ -219,8 +226,15 @@ std::istream& operator>>(std::istream& in, Song& obj){
     delete[] obj.genre;
     obj.genre=strcpy(new char[strlen(buffer)+1], buffer);
 
-    std::cout<<" Length: ";
-    in>>obj.length;
+    std::cout<<" Length(minutes): ";
+    while(!(in>>obj.length) || obj.length < 0){
+        in.clear();
+        in.ignore(1000, '\n');
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+        std::cout<<" Invalid input! Please enter a valid length: ";
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    }
     in.ignore();
 
     return in;
@@ -229,19 +243,19 @@ std::istream& operator>>(std::istream& in, Song& obj){
 std::ostream& operator<<(std::ostream& out, const Song& obj){
     out<<" Song #"<<obj.id<<'\n';
     if(obj.name != nullptr)
-        out<<" Song: "<<*obj.name<<'\n';
+        out<<" Song: "<<obj.name<<'\n';
     else    
         out<<"\n Song not registered!";
     if(obj.albumName != nullptr)
-        out<<" From the Album: "<<*obj.albumName<<'\n';
+        out<<" From the Album: "<<obj.albumName<<'\n';
     else    
         out<<"\n Album not registered!";
     if(obj.bandName != nullptr)
-        out<<" Band Name: "<<*obj.bandName<<'\n';
+        out<<" Band Name: "<<obj.bandName<<'\n';
     else    
         out<<"\n Band name not registered!";
     if(obj.genre != nullptr)
-        out<<" Genre: "<<*obj.genre<<'\n';
+        out<<" Genre: "<<obj.genre<<'\n';
     else    
         out<<"\n Genre not registered!";
     out<<" Length: "<<obj.length<<'\n';
@@ -250,7 +264,7 @@ std::ostream& operator<<(std::ostream& out, const Song& obj){
 
 class Review{
     private:
-        int static reviewCnt;
+        static int reviewCnt;
         const int id;
         float rating;
         char* date;
@@ -277,14 +291,14 @@ Review::Review() : id(reviewCnt++){
     this->recommend = 0; 
 }
 
-Review::Review(float rating, char* date, char* text, bool recommend) : id(reviewCnt){
+Review::Review(float rating, char* date, char* text, bool recommend) : id(reviewCnt++){
     this->rating = rating;
     this->date = strcpy(new char[11], date);
     this->text = strcpy(new char[strlen(text) + 1], text);
     this->recommend = recommend;
 }
 
-Review::Review(const Review& obj) : id(reviewCnt){
+Review::Review(const Review& obj) : id(reviewCnt++){
     this->rating = obj.rating;
     this->date = strcpy(new char[11], obj.date);
     this->text = strcpy(new char[strlen(obj.text)+1], obj.text);
@@ -300,6 +314,7 @@ Review& Review::operator=(const Review& obj){
     this->date = strcpy(new char[11], obj.date);
     delete[] text;
     this->text = strcpy(new char[strlen(obj.text)+1], obj.text);
+    this->recommend = obj.recommend;
     return *this;
 }
 
@@ -311,16 +326,19 @@ Review::~Review(){
 std::istream& operator>>(std::istream& in, Review& obj){
     char buffer[256];
     float rating;
-    bool ok;
+    bool ok = false;
 
+    std::cin.ignore();
     std::cout<<" Rating (out of 10): ";
-    in>>rating;
-    in.ignore();
-    while(rating < 0 || rating > 10){
+    while(!(in>>rating) || rating < 0 || rating > 10){
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
         std::cout<<" Invalid input! Try again: ";
-        in>>rating;
-        in.ignore();
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        in.clear();
+        in.ignore(1000, '\n');
     }
+    in.ignore();
     obj.rating = rating;
 
     std::cout<<" Date (dd-mm-yyyy): ";
@@ -334,20 +352,20 @@ std::istream& operator>>(std::istream& in, Review& obj){
 
     std::cout<<" Leave a review: ";
     in.getline(buffer, 256);
-    delete[] obj.date;
-    obj.date=strcpy(new char[strlen(buffer)+1], buffer);
+    delete[] obj.text;
+    obj.text=strcpy(new char[strlen(buffer)+1], buffer);
 
     std::cout<<" Do you recommend this album? ( Yes / No ): ";
     in.getline(buffer, 256);
-    while(ok == 0){
+    while(!ok){
         strupr(buffer);
         if(strcmp(buffer, "YES") == 0){
             obj.recommend = 1;
-            ok = 1;
+            ok = true;
         }
         else if(strcmp(buffer, "NO") ==0){
             obj.recommend = 0;
-            ok = 1;
+            ok = true;
         }
         else{
             std::cout<<" Invalid input! Try again: ";
@@ -362,11 +380,11 @@ std::ostream& operator<<(std::ostream& out, const Review& obj){
     out<<" Review #"<<obj.id<<'\n';
     out<<" Rating: "<<obj.rating<<'\n';
     if(obj.date != nullptr)
-        out<<" Date: "<<*obj.date<<'\n';
+        out<<" Date: "<<obj.date<<'\n';
     else    
         out<<"\n Band name not registered!";
     if(obj.text != nullptr)
-        out<<" Review: "<<*obj.text<<'\n';
+        out<<" Review: "<<obj.text<<'\n';
     else    
         out<<"\n No review paragraph registered!";
     out<<" Recommended: ";
@@ -380,19 +398,40 @@ std::ostream& operator<<(std::ostream& out, const Review& obj){
 
 class User{
     private:
-        int static userCnt;
+        static int userCnt;
         const int id;
         char* name;
+        int albumCnt;
         Album* albums[100];
+        int songCnt;
         Song* songs[100];
+        int reviewCnt;
         Review* reviews[100];
       
     public:
         User();
-        User(char* name);
+        User(char* name, int albumCnt, int songCnt, int reviewCnt);
         User(const User& obj);
         User& operator=(const User& obj);
         ~User();
+
+        void addAlbum(Album* p);
+        void addSong(Song* p);
+        void addReview(Review* p);
+
+        void displayAlbums();
+        void displaySongs();
+        
+        const int getAlbumCnt() const {return albumCnt;}
+        const int getSongCnt() const {return songCnt;}
+        const int getReviewCnt() const {return reviewCnt;}
+        void setAlbumCnt(int cnt) {albumCnt = cnt;}
+        void setSongCnt(int cnt) {songCnt = cnt;}
+        void setReviewCnt(int cnt) {reviewCnt = cnt;}
+
+        static int getUserCnt();
+        const char* getName() const;
+        void setName(char* name);
  
         friend std::istream& operator>>(std::istream& in, User& obj);
         friend std::ostream& operator<<(std::ostream& out, const User& obj);
@@ -407,15 +446,23 @@ User::User() : id(userCnt++){
         this->songs[i] = nullptr;
         this->reviews[i] = nullptr;
     } 
+
+    this->albumCnt = 0;
+    this->songCnt = 0;
+    this->reviewCnt = 0;
 }
 
-User::User(char* name) : id(userCnt++){
+User::User(char* name, int albumCnt, int songCnt, int reviewCnt) : id(userCnt++){
     this->name = strcpy (new char[strlen(name)+1], name);
     for(int i=0; i < 100; i++){ 
         this->albums[i] = nullptr;
         this->songs[i] = nullptr;
         this->reviews[i] = nullptr;
     }
+
+    this->albumCnt = 0;
+    this->songCnt = 0;
+    this->reviewCnt = 0;
 }
 
 User::User(const User& obj) : id(userCnt++){
@@ -425,6 +472,9 @@ User::User(const User& obj) : id(userCnt++){
         this->songs[i] = nullptr;
         this->reviews[i] = nullptr;
     }
+    this->albumCnt = obj.albumCnt;
+    this->songCnt = obj.songCnt;
+    this->reviewCnt = obj.reviewCnt;
 }
 
 User& User::operator=(const User& obj){
@@ -432,26 +482,113 @@ User& User::operator=(const User& obj){
         return *this;
     }
     delete[] name;
+    for(int i=0; i < 100; i++){
+        delete albums[i];
+        delete songs[i];
+        delete reviews[i];
+    }
     this->name = strcpy(new char[strlen(obj.name)+1], obj.name);
     for(int i=0; i < 100; i++){ 
         this->albums[i] = nullptr;
         this->songs[i] = nullptr;
         this->reviews[i] = nullptr;
     }
+    this->albumCnt = obj.albumCnt;
+    this->songCnt = obj.songCnt;
+    this->reviewCnt = obj.reviewCnt;
+
     return *this;
 }
 
 User::~User(){
     delete[] name;
+    for(int i=0;i<100;i++){
+        delete albums[i];
+        delete songs[i];
+        delete reviews[i];
+    }
+}
+
+void User::addAlbum(Album* p){
+    int cnt = getAlbumCnt();
+    if(cnt<100){
+        this->albums[cnt] = p;
+        setAlbumCnt(++cnt);
+    }
+    else{
+        std::cout<<"Album limit reached!\n";
+        delete p;
+    }
+}
+
+void User::addSong(Song* p){
+    int cnt = getSongCnt();
+    if(cnt<100){
+        this->songs[cnt] = p;
+        setSongCnt(++cnt);
+    }
+    else{
+        std::cout<<"Song limit reached!\n";
+        delete p;
+    }
+}
+
+void User::addReview(Review* p){
+    int cnt = getReviewCnt();
+    if(cnt<100){
+        this->reviews[cnt] = p;
+        setReviewCnt(++cnt);
+    }
+    else{
+        std::cout<<"Review limit reached!\n";
+        delete p;
+    }
+}
+
+void User::displayAlbums(){
+    int cnt = getAlbumCnt();
+    if(cnt == 0){
+        std::cout<<"The playlist is currently empty!\n";
+        return;
+    }
+    for(int i = 0; i < cnt; i++)
+        std::cout<<i<<". "<<*this->albums[i]<<'\n';
+}
+
+void User::displaySongs(){
+    int cnt = getSongCnt();
+    if(cnt == 0){
+        std::cout<<"The playlist is currently empty!\n";
+        return;
+    }
+    for(int i = 0; i < cnt; i++)
+        std::cout<<i<<". "<<*this->songs[i]<<'\n';
+}
+
+int User::getUserCnt(){
+    return userCnt;
+}
+
+const char* User::getName() const{
+    return name;
+}
+
+void User::setName(char* newName){
+    delete[] this->name;
+    this->name=strcpy(new char[strlen(newName)+1], newName);
 }
 
 std::istream& operator>>(std::istream& in, User& obj){
     char buffer[256];
 
-    std::cout<<" Username: ";
+    std::cout<<"Enter username: ";
     in.getline(buffer, 256);
     delete[] obj.name;
     obj.name=strcpy(new char[strlen(buffer)+1], buffer);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+    std::cout<<"\nNew user registered!\n";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 
     return in;
 }
@@ -459,9 +596,9 @@ std::istream& operator>>(std::istream& in, User& obj){
 std::ostream& operator<<(std::ostream& out, const User& obj){
     out<<" User #"<<obj.id<<'\n';
     if(obj.name != nullptr)
-        out<<" Title: "<<*obj.name<<'\n';
+        out<<" Name: "<<obj.name<<'\n';
     else    
-        out<<"\n No title registered!";
+        out<<"\n No user registered!";
   
     return out;
 }
@@ -477,8 +614,7 @@ class Menu{
         Menu() = default;
         ~Menu();
         void run();
-        void logIn();
-        void userMenu();
+        void userMenu(int index);
 };
 
 Menu::~Menu(){
@@ -487,20 +623,28 @@ Menu::~Menu(){
 
 void Menu::printUsers() const{
     if(users.empty()){
-        std::cout<<" No registered users!\n";
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+        std::cout<<"No registered users!";
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        std::cin.get();
         return;
     }
     for(size_t i = 0; i < users.size(); i++)
-        std::cout<<" "<<i<<". "<<users[i]<<'\n';
+        std::cout<<" "<<i<<". "<<*users[i]<<'\n';
 }
 
 int Menu::pickUser() const{
     if(users.empty()){
-        std::cout<<" No registered users!\n";
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+        std::cout<<"No registered users!";
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        std::cin.get();
         return -1;
     }
     printUsers();
-    std::cout<<" Pick user index (type -1 to cancel): ";
+    std::cout<<"Pick user index (type -1 to cancel): ";
     int index;
     std::cin>>index;
     std::cin.ignore();
@@ -510,9 +654,113 @@ int Menu::pickUser() const{
 
 void Menu::run(){
     while(true){
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
-        std::cout<<R"(                                                                         
+        std::cout<<"\n---Main Menu---\n\n";
+        std::cout<<"0 - Exit\n";
+        std::cout<<"1 - Log in\n";
+        std::cout<<"2 - Register\n";
+        std::cout<<"\nOption: ";
+
+        int option;
+        std::cin>>option;
+        std::cin.ignore();
+        std::cout<<'\n';
+
+        switch(option){
+            case 0:
+                std::cout<<"See you soon!\n";
+                return;
+            case 1:{
+                int index = pickUser();
+                if(index != -1) userMenu(index);
+                break;
+            }
+            case 2:{
+                User* p = new User();
+                std::cin>>*p;
+                users.push_back(p);
+                break;
+            }
+            default:{
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+                std::cout<<"Invalid option!\n";
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+            }
+        }
+    }
+}
+
+void Menu::userMenu(int index){
+    while(true){
+        std::cout<<"\n"<<*users[index]<<"\n";
+        std::cout<<"0 - Exit\n";
+        std::cout<<"1 - Edit username\n";
+        std::cout<<"2 - Favorite Songs playlist\n";
+        std::cout<<"3 - Favorite Albums playlist\n";
+        std::cout<<"4 - Add song to favorites\n";
+        std::cout<<"5 - Add album to favorites\n";
+        std::cout<<"6 - Reviews\n";
+        std::cout<<"\nOption: ";
+
+        int option;
+        std::cin>>option;
+        std::cin.ignore();
+        std::cout<<'\n';
+
+        switch(option){
+            case 0:
+                return;
+            case 1:{
+                std::cout<<"New username: ";
+                char aux[256];
+                std::cin.getline(aux, 256);
+                (*users[index]).setName(aux);
+                break;
+            }
+            case 2:{
+                std::cout<<"Favorite Songs:\n\n";
+                (*users[index]).displaySongs();
+                break;
+            }
+            case 3:{
+                std::cout<<"Favorite Albums:\n\n";
+                (*users[index]).displayAlbums();
+                break;
+            }
+            case 4:{
+                std::cout<<"";
+                Song* s = new Song();
+                std::cin>>*s;
+                (*users[index]).addSong(s);
+                break;
+            }
+            case 5:{
+                std::cout<<"";
+                Album* a = new Album();
+                std::cin>>*a;
+                (*users[index]).addAlbum(a);
+                break;
+            }
+            case 6:{
+                std::cout<<"";
+            }
+            default:{
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+                std::cout<<"Invalid option!\n";
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+            }
+
+        }
+    }
+}
+
+int main(){  
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);   
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+
+    std::cout<<R"(                                                                         
                                                                                                        $$$$$$"
              __       __      .______    __    __  .___  ___.                                        3$$$$$"
            /   \     |  |     |   _  \  |  |  |  | |   \/   |                                       z$$$$P
@@ -529,41 +777,12 @@ void Menu::run(){
         | _| `._____||_______|   \__/     |__| |_______|   \__/  \__/  |_______/             zP
                                                                                             z"
                                                                                            /
-        )";
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-        
-        std::cout<<" Main Menu\n\n";
-        std::cout<<"0 - Exit\n";
-        std::cout<<"1 - Log in\n";
-        std::cout<<"2 - Register\n";
-        std::cout<<"Option: ";
+    )";
 
-        int option;
-        std::cin>>option;
-        std::cin.ignore();
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 
-        switch(option){
-            case 0:
-                std::cout<<"See you soon!\n";
-                return;
-            case 1:
-                pickUser();
-                break;
-            case 2:{
-                User* p = new User();
-                std::cin>>*p;
-                users.push_back(p);
-                std::cout<<"New user registered!";
-                break;
-            }
-            default:
-                std::cout<<"Invalid option!\n";
-        }
-    }
-}
-
-int main(){  
     Menu menu;
     menu.run();
+
     return 0;
 }
